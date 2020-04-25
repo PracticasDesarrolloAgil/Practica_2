62d7ee717d8d40677f49f4a81cf6093d9c0d474f
abg00048 <60878087+abg00048@users.noreply.github.com>
2020-04-25 22:54:39 +0200

    Añadido "es.js"

---
+++ elimperiofinal/media/js/lang/es.js
@@ -0,0 +1,23 @@
+var es = {
+	terrible: "terrible",
+	poor: "pobre",
+	mediocre: "mediocre",
+	fair: "justo",
+	good: "bueno",
+	great: "excelente",
+	superb: "soberbio",
+	yes: "si",
+	no: "no",
+	choice: "Elección {number}",
+	no_group_definition: "No se ha podido encontrar una definición de grupo para {id}.",
+	link_not_valid: "El enlace '{link}' no parece ser válido.",
+	link_no_action: "Un link con la situación '.', debe tener una acción.",
+	unknown_situation: "No puedes moverte a una situación desconocida: {id}.",
+	existing_situation: "No puedes anular la situación {id} en HTML.",
+	erase_message: "Esto eliminará permanentemente este personaje y te devolverá inmediatamente al inicio del juego. ¿Estás seguro?",
+	no_current_situation: "No puedo mostrar, porque no tenemos una situación actual.",
+	no_local_storage: "No hay almacenamiento local disponible.",
+	random_seed_error: "Debe proporcionar una semilla aleatoria válida.",
+	random_error: "Inicialice el Random con una semilla no vacía antes de usar.",
+	dice_string_error: "No se pudo interpretar su cadena de dados: '{string}'."
+};
